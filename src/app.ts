f5443041a5ca92583af86d6ffe6146af22a8754b
brunovigo24 <brunohvigo@gmail.com>
2025-05-19 12:03:38 -0300

    feat: adicionar rota de destinos e implementar carregamento JSON para transfêrencia destinos

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,15 @@ import express from 'express';
 import webhookRoutes from './routes/webhookRoutes';
 import statusRoutes from './routes/statusRoutes';
 import mensagensRoutes from './routes/mensagensRoutes';
+import destinosRoutes from './routes/destinosRoutes';
 
 const app = express();
 app.use(express.json());
 
 app.use('/webhook', webhookRoutes);
 app.use('/api/mensagens', mensagensRoutes);
+app.use('/api/destinos', destinosRoutes);
+
 app.use('/', statusRoutes);
 
 export default app;
