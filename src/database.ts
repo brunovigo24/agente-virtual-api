8da877bd1e6ee6f6e649e36308e6860a70be04bb
brunovigo24 <brunohvigo@gmail.com>
2025-07-17 16:53:51 -0300

    chore: ajustar configuração do pool de conexão com o banco de dados para utilizar variáveis de ambiente diretamente

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '172.17.0.1',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '123',
-  database: process.env.DB_DATABASE || 'automacaobeta',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
   waitForConnections: true,
   connectionLimit: 10,
